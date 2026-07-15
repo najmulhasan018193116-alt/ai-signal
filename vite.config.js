413722f3064d34f5d4637fcdb5900704d7e4b814
najmulhasan018193116-alt <najmulhasan018193116@gmail.com>
2026-07-15 20:10:49 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -33,13 +33,6 @@ export default defineConfig({
   ],
   build: {
     minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
     outDir: 'dist'
   }
 });
-
